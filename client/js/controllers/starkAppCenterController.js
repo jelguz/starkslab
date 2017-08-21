3280fd0b2f9122fb2c75d68543ca29e01001c56f
Jelene Guzman <jeleneguzman@gmail.com>
2017-08-22 00:16:07 +0800

    amended starkAppCenter page to retrieve JSON from the webservice. -No access control header is present in the requested resource- error was encountered (tomcat problem)

--- client/js/controllers/starkAppCenterController.js
+++ client/js/controllers/starkAppCenterController.js
@@ -1,3 +1,7 @@
-myApp.controller('starkAppCenterController', function($scope) {
-	$scope.message = 'Look! I am an about page.';
+myApp.controller('starkAppCenterController', function($scope, $http) {
+$http
+	.get("http://localhost:8080/StarkLabWebService/rest/tools/get/rating/desc/4/1")
+	.then(function(response) {
+    	$scope.myData = response;
+    });
 });
