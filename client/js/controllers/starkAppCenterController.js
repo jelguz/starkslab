8eb40346345296fa7bbe1810e3d731c1a8cc870d
equser <equser@MANVSWBFEQ17.misys.global.ad>
2017-08-25 20:57:30 +0800

    consumed json in starkAppCenter page

--- client/js/controllers/starkAppCenterController.js
+++ client/js/controllers/starkAppCenterController.js
@@ -1,7 +1,7 @@
 myApp.controller('starkAppCenterController', function($scope, $http) {
 $http
-	.get("http://localhost:8080/StarkLabWebService/rest/tools/get/rating/desc/4/1")
+	.get("http://manvswbfeq17:8080/StarkLabWebService/rest/tools/get/rating/desc/12/1")
 	.then(function(response) {
-    	$scope.myData = response;
+    	$scope.myData = response.data;
     });
 });
