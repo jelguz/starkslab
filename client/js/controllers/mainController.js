63bb0d072820cb39c922a0c7e94270ac876cb409
Jelene Guzman <jeleneguzman@gmail.com>
2017-09-17 22:48:18 +0800

    added notifications

--- client/js/controllers/mainController.js
+++ client/js/controllers/mainController.js
@@ -1,3 +1,14 @@
-myApp.controller('mainController', function($scope, $rootScope) {
-	$scope.message = 'Everyone come and see how good I look!';
+myApp.controller('mainController', function($rootScope, $scope, $http, API_ENDPOINT) {
+
+
+            $http
+            .get(API_ENDPOINT.url + "/activity/notifications/" + $rootScope.globals.currentUser.username)
+            .then(function(response) {
+                $scope.myData = response.data;
+                console.log($scope.myData);
+            });
+        
+
+
 });
+
