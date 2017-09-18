59cc76cad87caae5ee8604625c60ef7d03c1a2b1
Isidro <DarylArvin.Isidro@misys.com>
2017-09-18 16:10:21 +0800

    Login Validation and Database Entry for Persons DB

--- client/js/controllers/loginController.js
+++ client/js/controllers/loginController.js
@@ -1,8 +1,8 @@
 'use strict';
   
 myApp.controller('loginController',
-    ['$scope', '$rootScope', '$location', 'AuthenticationService', '$http', 'API_ENDPOINT',
-    function ($scope, $rootScope, $location, AuthenticationService, $http, API_ENDPOINT) {
+    ['$scope', '$rootScope', '$location', 'AuthenticationService', '$http', 'API_ENDPOINT', '$q',
+    function ($scope, $rootScope, $location, AuthenticationService, $http, API_ENDPOINT, $q) {
         
   
         $scope.login = function () {
@@ -13,11 +13,16 @@ myApp.controller('loginController',
                 if(response.data.successfulLogin == true) {
                     AuthenticationService.SetCredentials(response.data.id, response.data.successfulLogin, 
                         response.data.firstName, response.data.lastName, response.data.email);
-					$scope.loadNotifs;
-                    $location.path('/');
+					PersonExistsInDB().then(function(response){
+						if (response == false) AddPersonToDB();
+						$scope.loadNotifs;
+						$location.path('/');
+					});
                 } else {
                     $scope.error = response.message;
                     $scope.dataLoading = false;
+					console.log("BAD LOGIN");
+					$("#loginErrorCollapse").collapse("show");
                 }
             });
         };
@@ -27,4 +32,32 @@ myApp.controller('loginController',
             $location.path('/login');
         };
 
+		function PersonExistsInDB(){
+			var deferred = $q.defer();
+			$http.get(API_ENDPOINT.url + '/persons/get/' + $rootScope.globals.currentUser.username)
+			.then(function(response){
+				if (response.data.length == 0) {
+					console.log("PERSON DOES NOT EXIST!");
+					deferred.resolve(false);
+				} else { 
+					console.log("PERSON ALREADY EXISTS!");
+					deferred.resolve(true);
+				}
+			});
+			return deferred.promise;
+		}
+		
+		function AddPersonToDB(){
+			var currentUser = $rootScope.globals.currentUser;
+			var person = {
+				id: currentUser.username,
+				firstName: currentUser.firstName,
+				lastName: currentUser.lastName,
+				middleName: "",
+			};
+			$http.post(API_ENDPOINT.url + '/persons/add',person)
+			.then(function(response){
+			});
+		}
+		
     }]);
